74b175f4081a4c999ba158b80a69032056f9370a
Reda HERRESS <reda.herress@gmail.com>
2025-02-11 15:00:46 +0100

    feat: initialize a SpringBoot project structure

---
+++ src/test/java/fr/hojde/ludy/FrontendControllerTests.java
@@ -0,0 +1,24 @@
+package fr.hojde.ludy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebMvcTest(FrontendController.class)
+public class FrontendControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void indexShouldReturnHtmlIndex() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(view().name("index"));
+    }
+}
